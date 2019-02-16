63d8b009c08b23ca0e8223a22edf4bc043d7c4d9
huangxiaoxiao <huangxiaoxiao4954@163.com>
2019-02-16 22:40:55 +0800

    add some change

--- code/SCurveSelf/SCurve.m
+++ code/SCurveSelf/SCurve.m
@@ -5,7 +5,7 @@ Amax = 100;
 Jmax = 1000;
 Vmax = 200;
 Vs = 10;
-Sinput = 400;
+Sinput = 500;
 %常量计算%
 T1 = Amax / Jmax;
 T2 = (Vmax - Vs - Amax * T1) / Amax;
@@ -14,7 +14,7 @@ if T2 < 0
 end
 %边界计算%
 Smin1 = 4 * Vs * T1 + 2 * Amax * T1^2;
-Smin2 = 4 * Vs * T1 + 2 * Vs * T2 + 3 * Amax * T1 * T2 + 2 * Amax * T1^2 + Amax * T2;
+Smin2 = 4 * Vs * T1 + 2 * Vs * T2 + 3 * Amax * T1 * T2 + 2 * Amax * T1^2 + Amax * T2^2;
 %情况一 位移最大%
 if Sinput > Smin2
     %---------------------------------T1---------------------------------------------%
@@ -49,9 +49,17 @@ if Sinput > Smin2
     plot(Tplot1,Vplot1,'b');
     ylim([Vs/5,Vmax + Vmax/10]);
     hold on
+    
+    %S的图像 T1%
+    Splot1 = zeros(1,T1/0.001);
+    for i = 1 : T1/0.001
+        Splot1(i) = Vs * Tplot1(i) + 1/6 * Jmax * Tplot1(i)^3;
+    end
+    S1 = Vs * T1 + 1/6 * Jmax * T1^3;
+    figure(4);
+    plot(Tplot1,Splot1,'k');
     %----------------------------------T1--------------------------------------------%
-    
-    
+       
     %---------------------------------T2---------------------------------------------%
     %J的图像 T2%
     ThetaT2 = T1;
@@ -77,6 +85,16 @@ if Sinput > Smin2
     end
     figure(3)
     plot(Tplot2,Vplot2,'b');
+    
+    %S的图像 T2%
+    Splot2 = zeros(1,T2/0.001);
+    for i = 1 : T2/0.001
+        Splot2(i) = S1 + (Vs + 1/2 * Jmax * T1^2) * (Tplot2(i) - ThetaT2) + 1/2 * Amax * (Tplot2(i) - ThetaT2)^2;
+    end
+    S2 = S1 + (Vs + 1/2 * Jmax * T1^2) * T2 + 1/2 * Amax * T2^2;
+    figure(4);
+    plot(Tplot2,Splot2,'k');
+    hold on;
     %---------------------------------T2---------------------------------------------%
     
     %---------------------------------T3---------------------------------------------%
@@ -109,6 +127,16 @@ if Sinput > Smin2
     figure(3)
     plot(Tplot3,Vplot3,'b');
     hold on;
+    
+    %S的图像 T3%
+    Splot3 = zeros(1,T3/0.001);
+    for i = 1 : T3/0.001
+        Splot3(i) = S2 + (Vs + 1/2 * Jmax * T1^2 + Amax * T2)*(Tplot3(i) - ThetaT3) + 1/2 * Amax * (Tplot3(i) - ThetaT3)^2 - 1/6 * Jmax * (Tplot3(i) - ThetaT3)^3;
+    end
+    S3 = S2 + (Vs + 1/2 * Jmax * T1^2 + Amax * T2) * T3 + 1/2 * Amax * T3^2 - 1/6 * Jmax * T3^3;
+    figure(4);
+    plot(Tplot3,Splot3,'k');
+    hold on;
     %---------------------------------T3---------------------------------------------%
    
     %---------------------------------T4---------------------------------------------%
@@ -137,6 +165,16 @@ if Sinput > Smin2
     end
     figure(3);
     plot(Tplot4,Vplot4,'b');
+    hold on;
+    
+    %S的图像 T4%
+    Splot4 = zeros(1,T4/0.001);
+    for i = 1 : T4/0.001
+        Splot4(i) = S3 + (Vs + Amax * T2 + Amax * T3) * (Tplot4(i) - ThetaT4);
+    end
+    S4 = S3 + (Vs + Amax * T2 + Amax * T3) * T4;
+    figure(4);
+    plot(Tplot4,Splot4,'k');
     hold on;
     %---------------------------------T4---------------------------------------------%
     
@@ -170,6 +208,16 @@ if Sinput > Smin2
     figure(3)
     plot(Tplot5,Vplot5,'b');
     hold on;
+    
+    %S的图像 T5%
+    Splot5 = zeros(1,T5/0.001);
+    for i = 1 : T5/0.001
+        Splot5(i) = S4 + (Vs + Amax * T2 + Amax * T3) * (Tplot5(i) - ThetaT5) - 1/6 * Jmax * (Tplot5(i) - ThetaT5)^3;
+    end
+    S5 = S4 + (Vs + Amax * T2 + Amax * T3) * T5 - 1/6 * Jmax * T5^3;
+    figure(4);
+    plot(Tplot5,Splot5,'k');
+    hold on;
     %---------------------------------T5---------------------------------------------%
     
     %---------------------------------T6---------------------------------------------%
@@ -192,7 +240,23 @@ if Sinput > Smin2
     hold on;
     
     %V的图像 T6%
+    Vplot6 = zeros(1,T6/0.001);
+    for i = 1 : T6/0.001
+        Vplot6(i) = Vs + Amax * T2 + Amax * T3 - 1/2 * Jmax *T5^2 - Amax * (Tplot6(i) - ThetaT6);
+    end
+    figure(3);
+    plot(Tplot6,Vplot6,'b');
+    hold on;
     
+    %S的图像 T6%
+    Splot6 = zeros(1,T6/0.001);
+    for i = 1 : T6/0.001
+        Splot6(i) = S5 + (Vs + Amax * T2 + Amax * T3 - 1/2 * Jmax * T5^2) * (Tplot6(i) - ThetaT6) - 1/2 * Amax * (Tplot6(i) - ThetaT6)^2;
+    end
+    S6 = S5 + (Vs + Amax * T2 + Amax * T3 - 1/2 * Jmax * T5^2) * T6 - 1/2 * Amax * T6^2;
+    figure(4);
+    plot(Tplot6,Splot6,'k');
+    hold on;
     %---------------------------------T6---------------------------------------------%
    
     %---------------------------------T7---------------------------------------------%
@@ -215,8 +279,40 @@ if Sinput > Smin2
     end
     figure(2);
     plot(Tplot7,Aplot7,'g');
-    hold on
+    hold on;
+    
+    %V的图像 T7%
+    Vplot7 = zeros(1,T7/0.001);
+    for i = 1 : T7/0.001
+        Vplot7(i) = Vs + Amax*T2 + Amax*T3 - 1/2*Jmax*T5^2 - Amax * T6 - Amax * (Tplot7(i) - ThetaT7) + 1/2 * Jmax * (Tplot7(i) - ThetaT7)^2;
+    end
+    figure(3);
+    plot(Tplot7,Vplot7,'b');
+    hold on;
+    
+    %S的图像 T7%
+    Splot7 = zeros(1,T7/0.001);
+    for i = 1 : T7/0.001
+        Splot7(i) = S6 + (Vs + Amax * T2 + Amax * T3 - 1/2 * Jmax * T5^2 - Amax * T6) * (Tplot7(i) - ThetaT7) - 1/2 * Amax * (Tplot7(i) - ThetaT7)^2 + 1/6 * Jmax * (Tplot7(i) - ThetaT7)^2;
+    end
+    figure(4);
+    plot(Tplot7,Splot7,'k');
+    hold on;
     %---------------------------------T7---------------------------------------------%
+end
+
+%情况二 位移最第二大%
+if Smin1 < Sinput && Sinput <= Smin2
+    %重新计算T2,Vmax输入Vmax无效%
+    a = Amax;
+    b = 2 * Vs + 3 * Amax * T1;
+    c = 4 * Vs * T1 + 2 * Amax * T1^2 -Sinput;
+    NewT2 = (-b + sqrt(b ^2 - 4 * a *c))/(2 * a);
+    NewT2 = NewT2 / 0.001;
+    NewT2 = floor(NewT2);
+    NewT2 = NewT2 * 0.001;
+    
+    
 end
 %%
 
